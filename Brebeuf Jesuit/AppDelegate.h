73eeb530bc12d248f696615b80dcf43852729d20
Alec Gorge <alecgorge@gmail.com>
2013-04-10 16:36:28 -0400

    Initial Commit

---
+++ Brebeuf Jesuit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Brebeuf Jesuit
+//
+//  Created by Alec Gorge on 4/10/13.
+//  Copyright (c) 2013 Alec Gorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Brebeuf Jesuit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Brebeuf Jesuit
+//
+//  Created by Alec Gorge on 4/10/13.
+//  Copyright (c) 2013 Alec Gorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
